c5e644ba3f22b0103ee2d7b3e7680874216d4f23
Alan Marvel <alan@mrvl.io>
2023-01-12 13:50:57 -0500

    AoC 2022 Day 6 Part 2

---
+++ 2022/06/part2/src/main.rs
@@ -0,0 +1,40 @@
+fn main() {
+    let path = std::path::Path::new("input");
+    let stream = std::fs::read_to_string(path).unwrap();
+
+    let mut freq = ['0'; 13];
+
+    for (i, curr) in stream.chars().enumerate() {
+        if i > 12 && no_dupes(&freq, curr) {
+            println!("{}", i + 1);
+            // println!("{:?} {}", freq, curr);
+            return;
+        } else {
+            freq.copy_within(1.., 0);
+            let last = freq.last_mut().unwrap();
+            *last = curr;
+        }
+    }
+}
+
+fn no_dupes(freq: &[char], curr: char) -> bool {
+    let mut dupes: u32 = 0; // bitwise check
+
+    let bit_position = (curr as u8) - ('a' as u8) + 1;
+    let mask: u32 = 1 << bit_position;
+
+    dupes = dupes | mask;
+
+    for el in freq {
+        let bit_position = (*el as u8) - ('a' as u8) + 1;
+        let mask: u32 = 1 << bit_position;
+        let result = dupes & mask;
+
+        if result > 0 {
+            return false;
+        }
+        dupes = dupes | mask;
+    }
+
+    return true;
+}
