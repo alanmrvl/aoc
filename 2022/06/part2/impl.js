04d259f606847c7321a62e9b5e571cd23698e3c8
Alan Marvel <alan@mrvl.io>
2023-01-12 15:39:16 -0500

    AoC 2022 Day 6 Part 2 - JS

---
+++ 2022/06/part2/impl.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+console.log('yolo');
+
+fs.readFile('input', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    const windowSize = 14;
+
+    process(data, windowSize);
+});
+
+function process(data, windowSize) {
+    const len = data.length - windowSize;
+
+    for (let i = 0; i < len; i++) {
+        if (!windowContainsDupes(data, i, windowSize)) {
+            console.log(i + windowSize);
+            return;
+        }
+    }
+}
+
+function windowContainsDupes(data, offset, windowSize) {
+    const a = 'a'.codePointAt(0);
+
+    let bitmap = 0;
+
+    for (let w = offset; w < offset + windowSize; w++) {
+        const char = data.codePointAt(w) - a;
+        const mask = 0x1 << char;
+
+        if (bitmap & mask) {
+           return true; 
+        }
+        
+        bitmap |= mask;
+    }
+
+    return false;
+}
