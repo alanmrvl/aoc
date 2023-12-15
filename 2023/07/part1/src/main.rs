d1475afbd580c0936c0781e61c3f81ac8673d164
Alan Marvel <alan@mrvl.io>
2023-12-15 03:51:00 -0500

    AoC 2023 Day 7 Part 1 -- cargo fmt

--- 2023/07/part1/src/main.rs
+++ 2023/07/part1/src/main.rs
@@ -30,7 +30,7 @@ impl HandType {
     fn from(hand: &Vec<usize>) -> Self {
         const OFFSET: usize = 2;
         let mut counts: [usize; 13] = [0; 13];
-        
+
         for card in hand {
             counts[*card - OFFSET] += 1;
         }
@@ -76,38 +76,40 @@ impl FromStr for Hand {
     type Err = ParseHandError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let hand = s.trim().chars().map(|c| match c {
-            '2'..='9' => c.to_digit(10).unwrap() as usize,
-            'T' => 10,
-            'J' => 11,
-            'Q' => 12,
-            'K' => 13,
-            'A' => 14,
-            _ => panic!(),
-        })
-        .collect::<Vec<usize>>();
+        let hand = s
+            .trim()
+            .chars()
+            .map(|c| match c {
+                '2'..='9' => c.to_digit(10).unwrap() as usize,
+                'T' => 10,
+                'J' => 11,
+                'Q' => 12,
+                'K' => 13,
+                'A' => 14,
+                _ => panic!(),
+            })
+            .collect::<Vec<usize>>();
 
         // get hand type
         let htype = HandType::from(&hand);
 
-        let result = Hand {
-            htype,
-            hand,
-        };
+        let result = Hand { htype, hand };
 
         return Ok(result);
     }
 }
 
 fn main() {
-    let mut hands_with_bids = INPUT.lines().map(|line| {
-        let (hand_raw, bid_raw) = line.split_once(' ').unwrap();
-        let hand = hand_raw.parse::<Hand>().unwrap();
-        let bid = bid_raw.parse::<usize>().unwrap();
+    let mut hands_with_bids = INPUT
+        .lines()
+        .map(|line| {
+            let (hand_raw, bid_raw) = line.split_once(' ').unwrap();
+            let hand = hand_raw.parse::<Hand>().unwrap();
+            let bid = bid_raw.parse::<usize>().unwrap();
 
-        return (hand, bid);
-    })
-    .collect::<Vec<_>>();
+            return (hand, bid);
+        })
+        .collect::<Vec<_>>();
 
     hands_with_bids.sort_by(|x, y| x.0.cmp(&y.0));
 
